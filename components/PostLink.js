f2060450f6950afc56f80897e242032a10a88b1a
yoojat <yooja.ty@gmail.com>
2019-03-10 19:17:37 +0900

    Pretty URLS

--- components/PostLink.js
+++ components/PostLink.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
 
 export default props => (
-  <Link href={`/post?title=${props.title}`}>
+  <Link href={`/post?title=${props.title}`} as={`/post/${props.title}`}>
+    {/* as는 실제로 유저가 보개 되는 것 */}
     <a>{props.title}</a>
   </Link>
 );
