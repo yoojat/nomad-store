7495496b6d45e3e148b62b7193de6ef563ac4451
yoojat <yooja.ty@gmail.com>
2019-03-10 19:49:16 +0900

    Styles in NextJs

--- components/PostLink.js
+++ components/PostLink.js
@@ -3,6 +3,13 @@ import Link from "next/link";
 export default props => (
   <Link href={`/post?title=${props.title}`} as={`/post/${props.title}`}>
     {/* as는 실제로 유저가 보개 되는 것 */}
-    <a>{props.title}</a>
+    <a>
+      {props.title}
+      <style jsx global>{`
+        body {
+          background-color: red;
+        }
+      `}</style>
+    </a>
   </Link>
 );
