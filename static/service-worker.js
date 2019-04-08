e95a318a0f4961e8fd624be135f25669f54c55c9
yoojat <yooja.ty@gmail.com>
2019-04-08 20:08:10 +0900

    Service Workers part Two

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,3 +1,18 @@
-setInterval(() => {
-  console.log("Hi im working");
-}, 5000);
+self.addEventListener("install", event => {
+  const offlinePage = new Request("/");
+  event.waitUntil(
+    fetch(offlinePage).then(response => {
+      return caches.open("nomad-store").then(cache => {
+        return cache.put(offlinePage, response);
+      });
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(error => {
+      return caches.open("nomad-store").then(cache => cache.match("/"));
+    })
+  );
+});
